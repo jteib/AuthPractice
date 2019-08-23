29a39eee8c2939ccdd63683195291cb8c3b0a0ff
jteib <27736224+jteib@users.noreply.github.com>
2019-08-23 13:50:21 -0400

    convert header to func comp

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './HeaderStyle.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./HeaderStyle.css";
 
-class Header extends Component {
-  renderLinks() {
-    if (this.props.authenticated) {
-      return (
-        <div>
-          <Link to="/signout">Sign Out</Link>
-          <Link to="/feature">Feature</Link>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/signin">Sign In</Link>
-        </div>
-      );
-    }
-  }
+const Header = () => {
+  const redux = useSelector(state => {
+    return { authenticated: state.auth.authenticated };
+  });
 
-  render() {
-    return (
-      <div className="header">
-        <Link to="/">Redux Auth</Link>
-        {this.renderLinks()}
+  const { authenticated } = redux;
+  const renderLinks = () =>
+    authenticated ? (
+      <div>
+        <Link to="/signout">Sign Out</Link>
+        <Link to="/feature">Feature</Link>
+      </div>
+    ) : (
+      <div>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/signin">Sign In</Link>
       </div>
     );
-  }
-}
 
-function mapStateToProps(state) {
-  return { authenticated: state.auth.authenticated };
-}
+  return (
+    <div className="header">
+      <Link to="/">Redux Auth</Link>
+      {renderLinks()}
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(Header);
+export default Header;
